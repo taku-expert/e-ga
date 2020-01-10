0790cb34e4a3f18baa6c49dce115ca4f3c664a25
atsu0203 <atsu0203@gmail.com>
2020-01-10 11:07:51 +0900

    initial-comimit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#index'
-  resources :homes, only: [:index]
+  resources :homes, only: [:index, :show]
 
 end
