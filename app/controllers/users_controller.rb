46bf3fed179d0fc2ff49cf1b4b3dd2c2ce0b2964
kisaragisyuuji <kairasuof03@yahoo.co.jp>
2020-01-10 15:31:57 +0900

    root path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def mypage
+    @user = User.find(current_user.id)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.reauire(:user).permit(:name, :age, :gendder)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'homes#index'
+   
   resources :homes, only: [:index]
+  resources :users, only: [:edit, :update]
+  get '/mypage', to: "users#mypage"
 
+  root to: 'homes#index'
 end
