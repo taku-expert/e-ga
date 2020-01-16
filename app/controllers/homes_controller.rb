8f9a1ecb7a90bbef96523298240801d68432a4f9
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2020-01-16 20:04:17 +0900

    fix

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,13 +4,17 @@ class HomesController < ApplicationController
     @genres = Genre.all
     @user = current_user
     @movies = Movie.all.order("created_at DESC")
-    @user_genre_id = @user.genre.id
+    # @user_genre_id = @user.genre.id
     @movie_genre = Movie.where(genre_id: "#{@user_genre_id}")
     @movie_genre_view = @movie_genre.all.order("created_at DESC")
   end
 
   def show
     @movie = Movie.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def getstarted
     @genres = Genre.all
   end
 
