da2cc4e09ec2da22de7d6ea7cf6823857cc369c3
jenettosyo <jenettosyo@gmail.com>
2020-01-16 17:12:00 +0900

    選んだジャンル表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,8 +4,8 @@ class HomesController < ApplicationController
     @genres = Genre.all
     @user = current_user
     @movies = Movie.all.order("created_at DESC")
-    user_genre_id = @user.genre.id
-    @movie_genre = Movie.where(genre_id: "#{user_genre_id}")
+    @user_genre_id = @user.genre.id
+    @movie_genre = Movie.where(genre_id: "#{@user_genre_id}")
     @movie_genre_view = @movie_genre.all.order("created_at DESC")
   end
 
